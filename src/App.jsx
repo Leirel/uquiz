0e119f56004019c4000fe9e6103f95330d99445d
HikiLei <y2508189@gmail.com>
2025-09-24 18:50:01 +0900

    오류 코드 수정 및 css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
-import Results from "./pages/Result";
+import Results from "./pages/Results";
 
 function App() {
     return (
