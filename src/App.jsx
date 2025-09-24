2bb795e1d40fc3dfcc286a2008699fce11b82ee8
HikiLei <y2508189@gmail.com>
2025-09-24 18:01:23 +0900

    pages 생성  App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import {routes, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Result";
+
 function App() {
-    return <></>;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz/:nickname" element={<Quiz />} />
+            <Route path="/results/:nickname" element={<Results />} />
+        </Routes>
+    );
 }
 
 export default App;
